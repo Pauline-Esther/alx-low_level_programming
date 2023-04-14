2d3fafe8a5a24d0061e52e83c4c72963c16e02b7
Pauline-Esther <paulynesther1@gmail.com>
2023-04-14 15:35:10 +0000

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * main- Returns a pointer to the allocated memory
+ *
+ * Return: Always 0
+ * /
+
+void *malloc_checked(unsigned int b)
+
+{
+	void *ptr;
+	ptr=malloc(b);
+	if (s != NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
