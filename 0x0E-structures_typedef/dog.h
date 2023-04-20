dc13dbc39389dbe67a2c72464c585c95cd720804
Pauline-Esther <paulynesther1@gmail.com>
2023-04-20 05:50:59 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  *     * @owner: Third member
  *      *
  *       * Description: Longer description
- *        */
+ */
 struct dog
 {
 		char *name;
@@ -18,7 +18,7 @@ struct dog
 
 /**
  *  * dog_t - typedef for struct dog
- *   */
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
